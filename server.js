1742846ad4f1d6fa0c811587577de0e3817a6637
parkerjustice <you@example.com>
2022-03-16 12:17:36 -0400

    added js

---
+++ server.js
@@ -0,0 +1,18 @@
+const useexpress = require('express');
+const usemongoose = require('mongoose');
+
+const app = useexpress();
+const PORT = process.env.PORT || 3001;
+
+app.use(useexpress.json());
+app.use(useexpress.urlencoded({ extended: true }));
+app.use(require('./routes'));
+
+usemongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+usemongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
