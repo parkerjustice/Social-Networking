5dd93c29ff0e77cb4af98cbf1f8c8480fa76c9c0
parkerjustice <you@example.com>
2022-03-16 12:08:42 -0400

    added more js

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const usersS = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+usersS.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', usersS);
+
+module.exports = User;
